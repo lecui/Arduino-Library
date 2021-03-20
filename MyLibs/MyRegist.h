ab86472d227647150ca4a5382fb25a0c10c209f4
lecui <37288595+lecui@users.noreply.github.com>
2021-03-21 00:13:16 +0300

    Update MyRegist.h

--- MyLibs/MyRegist.h
+++ MyLibs/MyRegist.h
@@ -22,32 +22,3 @@
 #define analogWritePin11(val) (sbi(TCCR2A, COM2A1), OCR2A = val)
 
 #endif
-
-// PINMODE -- DDR -> 0-вход  1-выход
-// digitalWrite -- PORT  -> Если пин настроен на выход то 0- низкий сигналб 1- высокий; если на вход то 1- вкл подтягивающий резистор
-// digitalRead -- PIN
-//
-// |PORTS|        PINS             |    HEX     |  Command
-// |  D  | 7 6 5 4 3 2 1 0         |  B00000000 |DDRD PORTD PIND
-// |  B  | x x 13 12 11 10 9 8     |  B00000000 |DDRB PORTB PINB
-// |  C  | RESET A5 A4 A3 A2 A1 A0 |  B0000000  |DDRC PORTC PINC
-//       ^
-//       |
-// HEX B76543210
-//------------------------------------------------------------------
-// 0 1 2 3 4 5 6 7 | 8 9 10 11 12 13 x x | A0 A1 A2 A3 A4 A5 |
-//       PD        |          PB         |          PC       |
-//------------------------------------------------------------------
-//  DDRB = B00000001;  -->  PinMode(8,OUTPUT);
-//  digitalRead :  if(PINB & B00100000)  -->  if(digitalRead(13) == 1)
-//  digitalWrite : sbi(PORTB,PB5) - высокий уровень ; cbi(PORTB,PB5) - низкий уровень
-//------------------------------------------------------------------
-//  Пример:
-//  void setup()
-//  {
-//    DDRD = B01000000;  --> установить PIN6 как выход
-//  }
-//  void loop()
-//  {
-//     sbi(PORTD, PB6);  --> выставление PIN6 в высокий уровень
-//  }
